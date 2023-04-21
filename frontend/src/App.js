19aafa570b3eee74ab27931cfefa4b35aebd026c
Dylan Durand <dylan.durand88@outlook.fr>
2023-04-21 12:32:16 +0200

    Install react-router-dom
    Manage routes of app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,30 @@
+// Import all components
+import NavBar from './Components/NavBar/NavBar'
+import Footer from './Components/Footer/Footer'
+// Import Pages
+import Home from './Pages/Home/Home'
+import Page404 from './Pages/Page404/Page404'
+import Profil from './Pages/Profil/Profil'
+import SignIn from './Pages/SignIn/SignIn'
+
+// Import React Router Dom to use Routes & Route methode
+import {Routes, Route, Navigate} from 'react-router-dom';
+
+
 import './App.css';
 
 function App() {
   return (
     <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/user' element={<Profil />} />
+        <Route path='/404' element={<Page404 />} />
+        <Route path='*' element={<Navigate to="404"/>} />
+      </Routes>
+      <Footer />
     </>
   );
 }
